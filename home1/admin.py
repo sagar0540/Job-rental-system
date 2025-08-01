b3d198a397414c7d828e06eae20303491d6d815b
SagarPandey <sagar.211233@ncit.edu.np>
2025-08-01 13:14:54 +0545

    done

--- home1/admin.py
+++ home1/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from home1.models import Job_information
-from home1.models import Job_information1
-from home1.models import addjobs
-from home1.models import contactus
+from home1.models import Job_information1;
+from home1.models import addjobs;
+from home1.models import contactus;
 from home1.models import save_form
 
 # Register your models here.
